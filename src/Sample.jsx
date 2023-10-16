808d89fdad19b7735ef3b7ebec7a6ff0218c4793
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-16 20:33:16 +0530

    done

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 
 const Sample = (props) => {
-  const smash = () => {
-    throw new Error('Oops! The application crashed');
-  };
-
-  return (
-    <>
-      <h3>Sample Component</h3>
-      <button className='btn btn-primary mt-4' onClick={smash}>
-        Crash!
-      </button>
-    </>
-  );
+  throw new Error('Oops!');
 };
 
 export default Sample;
