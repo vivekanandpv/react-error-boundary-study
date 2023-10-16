9fc445d6216b6c2195c6579411a6e1923941049a
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-16 19:20:01 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <>
+      <div className='p-5'>
+        <h2>React Error Boundary Study</h2>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default App;
