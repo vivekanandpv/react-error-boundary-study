052dacd7ddf82aaffb32eb314d3da122959ead1c
Vivekanand P V <vivekanandpv@outlook.com>
2023-10-16 20:33:00 +0530

    done

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//  error boundary catches only in production mode! hence npm run serve
+//  event handler throwing the error is not caught
+//  timeouts and other asynchronous errors are not caught as well
 const Sample = (props) => {
   throw new Error('Oops!');
 };
